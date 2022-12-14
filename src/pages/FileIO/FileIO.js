49a15fd94207c6bb18958880b1965132ad3cd747
ITRON\gnugroho <gnugroho@itron.com>
2022-12-14 19:00:15 +0700

    Add ajax and upload progress bar

--- src/pages/FileIO/FileIO.js
+++ src/pages/FileIO/FileIO.js
@@ -7,7 +7,7 @@ import { useState, useRef } from "react";
 const FileCard = (props) => {
   // console.log(props);
   return (
-    <div className={`${styles.fileCard}`} id={`file-form-${props.idx}`}>
+    <div className={`${styles.fileCard}`} id={`${props.idx}`}>
       <div style={{ width: "calc(100% - 40px)" }}>
         <table>
           <tbody>
@@ -15,21 +15,22 @@ const FileCard = (props) => {
               <th>Title</th>
               <td>
                 {/* <input type="text" placeholder=""></input> */}
-                <select>
+                <select id={`cardTitle_${props.idx}`}>
                   <option value="IMFW">IMFW</option>
                   <option value="CMFW">CMFW</option>
                   <option value="OTA">OTA</option>
+                  <option value="OTHER">OTHER</option>
                 </select>
               </td>
             </tr>
             <tr>
               <th>Filename</th>
-              <td>{props.filename}</td>
+              <td id={`cardFilename_${props.idx}`}>{props.filename}</td>
             </tr>
             <tr>
               <th>Description</th>
               <td>
-                <textarea></textarea>
+                <textarea id={`cardDescription_${props.idx}`}></textarea>
               </td>
             </tr>
           </tbody>
@@ -68,7 +69,6 @@ const FileIO = () => {
   const appendFile = (fileList) => {
     let _file_keys = [...files.keys];
     let _file_list = [...files.list];
-    console.log(files);
 
     for (let i = 0; i < Array.from(fileList).length; i++) {
       while (true) {
@@ -89,19 +89,111 @@ const FileIO = () => {
     setFiles({ keys: _key, list: _list });
   };
 
+  //POST AJAX FORM
   const uploadForm = async (m_form) => {
-    const response = await fetch("http://localhost:5000/frn/file/upload", {
-      method: "post",
-      body: m_form,
-    });
+    // const response = await fetch("http://localhost:5000/frn/file/upload", {
+    //   method: "post",
+    //   body: m_form,
+    // });
+    // console.log(response);
 
-    console.log(response);
+    // For reference
+    let ajax = new XMLHttpRequest();
+    // ajax.upload.addEventListener('progress')
+    ajax.upload.addEventListener("progress", progressHandler, false);
+    ajax.addEventListener("load", completeHandler, false);
+    ajax.addEventListener("load", completeHandler, false);
+    ajax.addEventListener("error", errorHandler, false);
+    ajax.addEventListener("abort", abortHandler, false);
+    ajax.open("POST", "http://localhost:5000/frn/file/upload");
+    ajax.send(m_form);
+  };
+
+  const progressHandler = (event) => {
+    _("loaded_n_total").innerHTML =
+      "Uploaded " + event.loaded + " bytes of " + event.total;
+    var percent = (event.loaded / event.total) * 100;
+    _("progressBar").value = Math.round(percent);
+    _("status").innerHTML = Math.round(percent) + "% uploaded... please wait";
+  };
+
+  const completeHandler = (event) => {
+    _("status").innerHTML = event.target.responseText;
+    _("progressBar").value = 0; //wil clear progress bar after successful upload
+  };
+
+  const errorHandler = (event) => {
+    _("status").innerHTML = "Upload Failed";
+  };
+
+  const abortHandler = (event) => {
+    _("status").innerHTML = "Upload Aborted";
+  };
+
+  const _ = (el) => {
+    return document.getElementById(el);
   };
 
   const handlerUploadData = () => {
+    let projectName = document.getElementById("input-project-name").value;
     let m_form = new FormData();
-    m_form.append("coba 1", "hello world coba 1");
-    m_form.append("file", files);
+    let dummyPayload = {
+      Title: {
+        VERSION: "08.26",
+        NLR: "08.26",
+        NLR2: "08.24",
+        LR: "08.24",
+      },
+
+      Table: {
+        "Release Date": "2 January 1997",
+        Signatures: {
+          NLR: "Click here for NLR",
+          NLR2: "Click here for NLR2",
+          LR: "Click here for LR",
+        },
+        Notes: "Install via USB",
+      },
+    };
+
+    m_form.append("FRN", JSON.stringify(dummyPayload));
+    m_form.append("Project Name", projectName);
+    m_form.append("Pulblisher", "Anonym");
+
+    // If File attached
+    if (files.keys.length > 0) {
+      // Fill images information
+      let imageInformation = [];
+      for (let i = 0; i < files.keys.length; i++) {
+        let _key = files.keys[i];
+        let cardTitleSelection = document.getElementById(
+          `cardTitle_${_key}`
+        ).value;
+        let cardFilename = document.getElementById(
+          `cardFilename_${_key}`
+        ).textContent;
+        let cardDescription = document.getElementById(
+          `cardDescription_${_key}`
+        ).value;
+        console.log(cardTitleSelection);
+        console.log(cardFilename);
+        console.log(cardDescription);
+
+        imageInformation.push({
+          cardTitle: cardTitleSelection,
+          cardFilename: cardFilename,
+          cardDescription: cardDescription,
+        });
+      }
+
+      // m_form.append("ImagesInformation", JSON.stringify(imageInformation));
+
+      for (let i = 0; i < files.list.length; i++) {
+        // console.log(`add form #${i}`);
+        m_form.append("file", files.list[i]);
+      }
+    }
+
     uploadForm(m_form);
   };
 
@@ -137,7 +229,11 @@ const FileIO = () => {
           <div className={`${styles.cardWrapperPanel}`}>
             <div>
               <label htmlFor="">Project Name </label>
-              <input type="text" placeholder="Project Name"></input>
+              <input
+                id="input-project-name"
+                type="text"
+                placeholder="Project Name"
+              ></input>
             </div>
             <button className={`${styles.closeButton}`}>Clear</button>
           </div>
@@ -160,6 +256,18 @@ const FileIO = () => {
             Submit
           </button>
         </div>
+      </div>
+
+      <div className={`${styles.progressBar} ${styles.hide}`}>
+        <progress
+          id="progressBar"
+          value="0"
+          max="100"
+          style={{ width: "300px" }}
+        ></progress>
+        <br></br>
+        <h3 id="status"> </h3>
+        <p id="loaded_n_total"></p>
       </div>
     </>
   );
